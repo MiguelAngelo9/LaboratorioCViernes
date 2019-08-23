6c12edc7a0612e8ead535dbbe89fffdf268b6887
MiguelAngelo9 <miguel.solis@live.com.mx>
2019-08-23 08:59:13 -0500

    Primer commit Practica1

--- main.c
+++ main.c
@@ -4,5 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int numero = 1;
+	printf("El numero es: %d", numero);
 	return 0;
 }
