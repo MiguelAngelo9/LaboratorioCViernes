c3fbae8a667a5e7cfd54f71e8aad637f32262d72
MiguelAngelo9 <miguel.solis@live.com.mx>
2019-09-20 20:01:19 -0500

    For

--- main.c
+++ main.c
@@ -10,22 +10,50 @@
 */
 /*******************************************************************ACTIVIDAD #1************************************************************************************************************************/
 int main(int argc, char *argv[]) {
+	/* Utilice variables como calif de calificacion para saber que iba a ingresar el usuario, sum de suma para ir sumando lo que iba ingresando, prom de promedio para alfinar despues de
+	haber sumado todo, sacar el promedio, con de un contador, c para la variable que llevaria la cuenta de las calificaciones ingresadas*/
 	printf("\t\tActividad #1");
-	printf("\n\nIngrese las 10 calificaciones que obtuvo el alumno");
-	int c=0;
-	for(c=0; c<=10; c++){
+	printf("\n\nIngrese las 10 calificaciones que obtuvo el alumno calificando de 0 a 10");
+	int calif, sum, prom, con, c;
+	calif=0; 
+	sum=0;
+	prom=0;
+	c=0;
+	con=0;
+	/*Utilice un for empezando con c=1 por que empezaba introduciendo la primer calificacion, c<=10 por que eran las que le iba a pedir*/
+	for(c=1; c<=10; c++){
 	printf("\n\nCalificacion: ");
-	scanf("%d", &c);}
-	while(c>7){
-	printf("APROBADO");
+	scanf("%d", &calif);
+	sum+=calif;
+	con++;
+	prom=(sum/10);
+	printf("\n\nSu promedio es: %d", prom);}
+	/*El if y el else, para mandar a imprimir aprobado o reprobado*/
+	if(prom>7){
+	printf("\n\nAPROBADO");}
+	else{
+	printf("\n\nREPROBADO");
 	}
-	
 	printf("\n\n\n\t\tAqui Termina la Actividad ");
 /***************************************************************************************************************************Terminando Act #1******************************************************************************************/
 int a=2;
 printf("\n\n\nIngrese el numero 2 para pasar a la siguiente Actividad: ", a);
 scanf("%d", &a);
 /****************************************************************************************************************************ACTIVIDAD #2****************************************************************************************/
+ printf("\n\n\n\t\tActividad #2");
+ printf("\n\nIngrese un numero base y otro que sera el exponente");
+ int ba, ex, res, e;
+ res=1;
+ printf("\n\nIntroduzca un numero base: ");
+ scanf("%d", &ba);
+ printf("Introduzca un numero exponente: ");
+ scanf("%d", &ex);
+ for(e=1; e<=ex; e++){
+res=res*ba;
+ }
  
+ printf("\n\nEl resultado de multiplicar %d por si mismo %d veces es igual a: %d", ba, ex, res);
+ printf("\n\n\nAqui termina la Actividad #2");
+ /****************************************************************************************************************************Terminando Act #2************************************************************************************************************************/
 	return 0;
 }
