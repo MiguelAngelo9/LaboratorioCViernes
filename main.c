e9309204580715bd632e16ecbc9c43aa69e2c5ce
MiguelAngelo9 <miguel.solis@live.com.mx>
2019-10-04 08:50:09 -0500

    Array

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*
+*Autor: Miguel Angel Fernandez Solis
+*Fecha: 04/10/2019
+*Grupo: LaboratorioCViernes
+*Horario: Viernes 7am-9am
+*Matricula: 1822515*/
 
 int main(int argc, char *argv[]) {
+/***********************************************************************ACTIVIDAD #1*******************************************************/	
+	printf("\t\tActividad #1");
+	int veoe[6][10][2];
+	veoe[3][7][1]=4234;
+	printf("\n\nEl empleado numero 3 de la ofixina numero 7 del edificio numero 2 ha vendido %d unidades", veoe[3][7][1]);
+/**********************************************************************TERMINA ACTIVIDAD #1************************************************/
+    printf("\n\n\t\tActividad #2");
+    int veoe[6][10][2];
+    
+    
 	return 0;
 }
