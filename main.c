04090811c84cf8bcdf224f3935f2eaa7fab7dbcd
MiguelAngelo9 <miguel.solis@live.com.mx>
2019-10-04 20:12:32 -0500

    Array2

--- main.c
+++ main.c
@@ -11,13 +11,17 @@
 int main(int argc, char *argv[]) {
 /***********************************************************************ACTIVIDAD #1*******************************************************/	
 	printf("\t\tActividad #1");
-	int veoe[6][10][2];
+	int veoe[6][10][1];
 	veoe[3][7][1]=4234;
-	printf("\n\nEl empleado numero 3 de la ofixina numero 7 del edificio numero 2 ha vendido %d unidades", veoe[3][7][1]);
+	printf("\n\nEl empleado numero 3 de la oficina numero 7 del edificio numero 2 ha vendido: %d unidades", veoe[3][7][1]);
 /**********************************************************************TERMINA ACTIVIDAD #1************************************************/
+
+/***********************************************************************ACTIVIDAD #2*********************************************************/
     printf("\n\n\t\tActividad #2");
-    int veoe[6][10][2];
-    
-    
+    int e;
+    e=(4234+10);
+    for(e=0;e<11;e++)
+    printf("\n\nEl empleado numero 3 de la oficina numero 7 del edificio numero 2 ha vendido: %d unidades", veoe[3][7][1]+e);
+/***********************************************************************TERMINA ACTIVIDAD #2************************************************/
 	return 0;
 }
